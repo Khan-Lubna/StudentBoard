b6116fb7940604b93fddd6a8683103d171c00dba
Khan-Lubna <116883265+Khan-Lubna@users.noreply.github.com>
2025-02-05 01:20:11 +0530

    Add files via upload

---
+++ pages/LoginPage.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("Login successful!");
+      console.log("Login attempted with email:", email);
+      navigate("/students");
+    } catch (err) {
+      setError("Invalid username or password");
+      console.error("Login error: ", err.message);
+    }
+  };
+
+  return (
+    <div style={{ maxWidth: "400px", margin: "auto", textAlign: "center" }}>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          style={{ marginBottom: "10px", padding: "8px", width: "100%" }}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          style={{ marginBottom: "10px", padding: "8px", width: "100%" }}
+        />
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <button
+          type="submit"
+          style={{
+            background: "#007BFF",
+            color: "#fff",
+            border: "none",
+            padding: "10px 20px",
+            cursor: "pointer",
+          }}
+        >
+          Login
+        </button>
+      </form>
+      <div className="credentials">
+        <h3>Test Credentials:</h3>
+        <p>Email:admin@123.com</p>
+        <p>Password:admin@123</p>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
